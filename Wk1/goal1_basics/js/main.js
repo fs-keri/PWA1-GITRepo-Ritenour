1d6dda6b8c7672d6dfe10d0603ffce2667250f07
fs-keri <kritenour@fullsail.edu>
2015-09-09 18:05:55 -0400

    Arrays

--- Wk1/goal1_basics/js/main.js
+++ Wk1/goal1_basics/js/main.js
@@ -209,7 +209,10 @@ console.log('------ arrays ----------');
  */
 
     //arrays can hold any data type
-
+    console.log("index 0: ", myArr[0]);
+    console.log("index 1: ", myArr[1]);
+    console.log("index 2: ", myArr[2]);
+    console.log("index 3: ", myArr[3]);
 
 
  /*******************************************
@@ -221,7 +224,8 @@ console.log('------ arrays ----------');
 
  ********************************************/
 
-
+    var myArr1=[10, 50, 100, 200];
+    console.log("My Array Sum: ", myArr1[0] + myArr1[1] + myArr1[2] + myArr1[3]);
 
 /*******************************************
 
@@ -237,6 +241,13 @@ console.log('------ arrays ----------');
     3. output array items using string concatenation
 
  ********************************************/
+    var myArray = new Array();
+
+    myArray[0] = "baseball";
+    myArray[1] = "football";
+    myArray[2] = "basketball";
+
+    console.log(myArray[0] + ", " +myArray[1]+ ", "+myArray[2]);
 
 
  // conditionals: (definition, indexing, math, if, if-else, if-else if, ternary)
