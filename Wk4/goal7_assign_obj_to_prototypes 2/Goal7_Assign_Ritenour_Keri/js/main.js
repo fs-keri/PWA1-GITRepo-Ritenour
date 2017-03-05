2292c306c0974f48cebc6da4a5b0a3aed9d7fd13
fs-keri <kritenour@fullsail.edu>
2017-03-05 17:48:35 -0500

    Final

---
+++ Wk4/goal7_assign_obj_to_prototypes 2/Goal7_Assign_Ritenour_Keri/js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Name: Keri Ritenour
+ * Date: 9/27/2015
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+//Create an array called Names and populate with people names
+
+
+//Create a for loop that randomly selects 3 instances of the people object
+
+
+//Create an array called People that will store names selected from th
+
+
+//Instantiate the Person object; pass the random name and row number to the constructor
+
+//Ensure no duplicate names are selected
+
+//Create an Interval to call after a specified time of 1 every 30 seconds
+
+//Pass the results of the loop to populate the html corresponding to row and column number
---
+++ Wk4/goal7_assign_obj_to_prototypes 2/Goal7_Assign_Ritenour_Keri/js/person.js
@@ -0,0 +1,16 @@
+/**
+ * Name: Keri Ritenour
+ * Date: 9/27/2015
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+
+//Create the Person Constructor using name and row number
+
+
+//Create the prototype for the Person name, action, job and row
+
+
+//populate HTML
